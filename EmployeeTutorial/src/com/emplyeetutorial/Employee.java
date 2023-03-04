1081d0de6ee7a96eb505f20209d0ef8d4b9b60eb
MD AFTAB ALAM <111672590+mdaftabdeveloper@users.noreply.github.com>
2023-03-04 20:11:02 +0530

    Add files via upload

---
+++ EmployeeTutorial/src/com/emplyeetutorial/Employee.java
@@ -0,0 +1,36 @@
+package com.emplyeetutorial;
+
+public class Employee {
+
+	
+
+	
+		public int id;
+		public String name;
+		public Double salary;
+		
+		//parameterized constructor
+		public Employee(int id, String name, Double salary) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.salary = salary;
+			
+			//getters & setters 
+		}
+
+		public Employee(int id2, String name2, int i) {
+			// TODO Auto-generated constructor stub
+		}
+
+	
+
+		//to string methods
+		
+		
+	
+		
+
+	}
+
+
---
+++ EmployeeTutorial/src/com/emplyeetutorial/EmployeeTest.java
@@ -0,0 +1,15 @@
+package com.emplyeetutorial;
+public class EmployeeTest {
+
+	public EmployeeTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e1 = new Employee(1,"San", 10000);
+		System.out.println(e1);
+
+	}
+
+}
